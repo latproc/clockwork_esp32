c523b1757745d40aa8efc0c5cfb5d4e4df7b776f
Mike O'Connor <mike@oeg.com.au>
2019-01-13 20:58:37 +1030

    Deleted auto generated main/cw_ files

--- main/project.h
+++
@@ -1,7 +0,0 @@
-#include "cw_DINPUT.h"
-#include "cw_DebouncedInput.h"
-#include "cw_Pulse.h"
-#include "cw_Ramp.h"
-#include "cw_SpeedSelect.h"
-#include "cw_message_ids.h"
-#include "cw_symbol_ids.h"
