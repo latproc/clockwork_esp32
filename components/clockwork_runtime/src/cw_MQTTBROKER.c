26b76b69cc248559b90d701588ac87871c0ee486
Mike O'Connor <mike@hpontheair.fritz.box>
2019-01-17 01:19:17 +1030

    Added default case for MQTT event handling

--- components/clockwork_runtime/src/cw_MQTTBROKER.c
+++ components/clockwork_runtime/src/cw_MQTTBROKER.c
@@ -103,6 +103,7 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
         case MQTT_EVENT_ERROR:
             ESP_LOGI(TAG, "MQTT_EVENT_ERROR %s", context->machine.name);
             break;
+	default: ;
     }
     return ESP_OK;
 }
